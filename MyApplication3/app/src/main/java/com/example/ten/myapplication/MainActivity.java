3f68b0eb735eb5d6ea88ffc87f18c4acff1fd52c
Yeori <yoeri1123@naver.com>
2018-05-24 20:16:01 +0900

    [김여리][MapActivity추가+Btn클릭 이벤트 추가][20:15]

--- MyApplication3/app/src/main/java/com/example/ten/myapplication/MainActivity.java
+++ MyApplication3/app/src/main/java/com/example/ten/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.ten.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void ClickBtn(View view) {
+        switch (view.getId()){
+            case R.id.joinBtn:
+                //혜진
+                break;
+            case R.id.ApiBtn:
+                //혜지&상규
+                break;
+            case R.id.MapBtn:
+                Intent intent=new Intent(this, ShowMapActivity.class);
+                startActivity(intent);
+                //열
+                break;
+            default:
+                break;
+        }
     }
 }
